9cdd2fd80ce1e07b8eb5addeca09966d32be692c
david lin <woei66@gmail.com>
2017-04-17 22:56:33 +0800

    fix

--- app/src/main/java/com/optoma/launcher/home.java
+++ app/src/main/java/com/optoma/launcher/home.java
@@ -8,11 +8,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class test extends Activity {
+public class home extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.home);
     }
 }
