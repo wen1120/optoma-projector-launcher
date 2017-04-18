54b29b68a1714dd1d0ff8c9082b726286ff8bc5e
david lin <woei66@gmail.com>
2017-04-18 10:25:37 +0800

    create a test.xml layout for testing

--- app/src/main/java/com/optoma/launcher/home.java
+++ app/src/main/java/com/optoma/launcher/home.java
@@ -12,6 +12,6 @@ public class home extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+        setContentView(R.layout.test);
     }
 }
