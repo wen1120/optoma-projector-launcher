be03dd9d70f4879837fc7802efcd8c720dcca359
david lin <woei66@gmail.com>
2017-04-19 19:45:12 +0800

    imageview to android.support.v7.widget.AppCompatImageView

--- app/src/main/java/com/optoma/launcher/home.java
+++ app/src/main/java/com/optoma/launcher/home.java
@@ -12,6 +12,6 @@ public class home extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_menu);
+        setContentView(R.layout.home);
     }
 }
