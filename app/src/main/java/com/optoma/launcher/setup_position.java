6d5ef632f808b267828f0eb985d35a9354c2d421
KUAN-YING CHOU <kuanyingchou@gmail.com>
2017-05-05 11:09:57 +0800

    add focus effect

--- app/src/main/java/com/optoma/launcher/setup_position.java
+++ app/src/main/java/com/optoma/launcher/setup_position.java
@@ -1,11 +1,39 @@
 package com.optoma.launcher;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
 
 /**
  * Created by linweiting on 2017/4/20.
  */
 
-public class setup_position {
+public class setup_position extends Activity {
+    private static final int[] positionIds = new int[] {
+            R.id.front,
+            R.id.rear,
+            R.id.front_celling,
+            R.id.rear_ceiling
+    };
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setup_position);
+
+        findViewById(R.id.position).requestFocus();
+
+        for(int i=0; i<positionIds.length; i++) {
+            final View v = findViewById(positionIds[i]);
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final Intent intent = new Intent(setup_position.this, home.class);
+                    startActivity(intent);
+                }
+            });
+        }
+    }
 }
