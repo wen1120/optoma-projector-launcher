574d5b9a2ba7a66dbbe73af152868401007d3c29
KUAN-YING CHOU <kuanyingchou@gmail.com>
2017-05-16 16:45:29 +0800

    add model

--- app/src/main/java/com/optoma/launcher/Projector.java
+++ app/src/main/java/com/optoma/launcher/Projector.java
@@ -5,4 +5,100 @@ package com.optoma.launcher;
  */
 
 public class Projector {
+
+    public static final String[] displayModes = new String[]{
+            "[None]",
+            "Presentation",
+            "sRGB",
+            "DICOM SIM.",
+            "Bright",
+            "Game",
+            "User",
+            "3D",
+            "Cinema",
+            "Reference",
+            "Vivid",
+            "Film",
+            "User (3D)",
+            "ISF Day",
+            "ISF Night",
+            "ISF 3D",
+            "2D High Speed",
+            "Blending"
+    };
+
+    public static final String[] wallColors = new String[]{
+            "Off",
+            "BlackBoard",
+            "Light Yellow",
+            "Light Green",
+            "Light Blue",
+            "Pink",
+            "Gray"
+    };
+
+    public static final String[] brightnessModes = new String[]{
+            "Bright",
+            "Eco+",
+            "Dynamic",
+            "Eco",
+            "Constant Power",
+            "Constant Luminace"
+    };
+
+    public static final String[] gammaModes = new String[]{
+            "Film",
+            "Video",
+            "Graphics",
+            "Standard(2.2)",
+            "1.8",
+            "2.0",
+            "2.4",
+            "2.6",
+            "3D",
+            "Blackboard",
+            "DICOM SIM.",
+            "Bright",
+            "CRT",
+            "User"
+    };
+
+    public static final String[] colorTemperatures = new String[] {
+            "Warm",
+            "Standard",
+            "Cool",
+            "Cold",
+            "D55",
+            "D65",
+            "D75",
+            "D83",
+            "D93",
+            "Native (Bright)",
+            "Reset"
+    };
+
+    public static final String[] colorSettingsColors = new String[] {
+            "R",
+            "G",
+            "B",
+            "C",
+            "Y",
+            "M",
+            "W"
+    };
+
+    public static final String[] aspectRatios = new String[]{
+            "[None]",
+            "4:3",
+            "16:9",
+            "16:10",
+            "LBX",
+            "Superwide",
+            "Native",
+            "Auto",
+            "Auto235",
+            "Auto235_Subtitle",
+            "Auto 3D"
+    };
+
 }
