fe56fe9262c57b1625ccfd8ea429afd422c4ced9
KUAN-YING CHOU <kuanyingchou@gmail.com>
2017-06-07 16:33:30 +0800

    add comment

--- app/src/main/java/com/optoma/launcher/Util.java
+++ app/src/main/java/com/optoma/launcher/Util.java
@@ -7,6 +7,8 @@ public class Util {
 
     public static int dp(Context context, int px) {
         // from https://developer.android.com/guide/practices/screens_support.html#dips-pels
+        // note that the accepted answer on stackoverflow is wrong:
+        //   https://stackoverflow.com/questions/8309354/formula-px-to-dp-dp-to-px-android#
         return (int) (px * context.getResources().getDisplayMetrics().density + 0.5f);
     }
 }
